25fa4dd756cbc2fa8ebb280793e40a29320752c8
Mahmood28 <mahmoodalwatani@gmail.com>
2021-04-23 14:05:14 +0300

    styles file and main component

---
+++ components/ActivityDetails.js/index.js
@@ -0,0 +1,110 @@
+import React, { useRef, useState } from "react";
+// Data
+import data from "./data";
+// Components
+import Header from "./Header";
+import TopSection from "./TopSection";
+import BottomSection from "./BottomSection";
+import UpperStickyButton from "../buttons/UpperStickyButton";
+import LowerStickyButton from "../buttons/LowerStickyButton";
+// Styling
+import {
+  View,
+  Text,
+  Alert,
+  Image,
+  ScrollView,
+  Dimensions,
+  Animated,
+  Pressable,
+} from "react-native";
+import styles from "./styles";
+
+const { width, height } = Dimensions.get("screen");
+
+const ActivityDetails = () => {
+  const [bottomActions, setBottomActions] = useState(null);
+  const [image, setImage] = useState(null);
+  const [favourite, setFavourite] = useState(false);
+  const scrollY = useRef(new Animated.Value(0)).current;
+
+  const topEdge = bottomActions?.y - height + bottomActions?.height;
+  const bottomEdge = bottomActions?.y + bottomActions?.height;
+  const imageBottom = image?.y + image?.height - 60;
+
+  const activityImages = data.images.map((image, index) => (
+    <Image
+      key={`image-${index}`}
+      style={{
+        height: height * 0.6,
+        width,
+        margin: 0,
+        resizeMode: "cover",
+      }}
+      source={{
+        uri: image,
+      }}
+    />
+  ));
+
+  return (
+    <>
+      <Animated.ScrollView
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        style={styles.container}
+      >
+        <ScrollView
+          style={{ flex: 1 }}
+          horizontal={true}
+          pagingEnabled
+          showsHorizontalScrollIndicator
+          onLayout={(event) => {
+            setImage(event.nativeEvent.layout);
+          }}
+        >
+          {activityImages}
+        </ScrollView>
+        <TopSection />
+        <View
+          style={[styles.details, { height: 50 }]}
+          onLayout={(event) => {
+            setBottomActions(event.nativeEvent.layout);
+          }}
+        >
+          <Pressable
+            onPress={() => Alert.alert("Booking Page")}
+            style={styles.bookingButton}
+          >
+            <Text
+              style={{ color: "#279f27", fontSize: 16, fontWeight: "bold" }}
+            >
+              BOOK NOW
+            </Text>
+          </Pressable>
+        </View>
+        <BottomSection />
+      </Animated.ScrollView>
+      {bottomActions && (
+        <>
+          <UpperStickyButton topEdge={topEdge} scrollY={scrollY} />
+          <LowerStickyButton bottomEdge={bottomEdge} scrollY={scrollY} />
+        </>
+      )}
+      {image && (
+        <>
+          <Header
+            background={true}
+            imageBottom={imageBottom}
+            scrollY={scrollY}
+          />
+          <Header favourite={favourite} setFavourite={setFavourite} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default ActivityDetails;
