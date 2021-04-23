d7dffe9318763f86738af83bb657d8e4a83ab992
Mahmood28 <mahmoodalwatani@gmail.com>
2021-04-23 14:04:26 +0300

    dummy data

---
+++ components/ActivityDetails.js/data.js
@@ -0,0 +1,21 @@
+const data = {
+  title: "Football Fields",
+  organisation: "A'ali Sports Club",
+  category: "Football",
+  rating: 5,
+  days: ["Sunday", "Monday", "Tuesday", "Thursday", "Saturday"],
+  images: [
+    "https://guardian.ng/wp-content/uploads/2020/09/football-897x598.jpg",
+    "https://www.foreverlawn.com/wp-content/uploads/2019/01/DSC_0008.jpg",
+    "https://usi.edu/sports/webphotos/Soccer_Field/Strassweg2.jpg",
+  ],
+  details:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nDonec massa sapien faucibus et molestie. Varius duis at consectetur lorem.\nHabitant morbi tristique senectus et netus et malesuada. Viverra maecenas accumsan lacus vel facilisis volutpat. Id consectetur purus ut faucibus. Cursus sit amet dictum sit amet justo. Nec ultrices dui sapien eget mi proin sed libero. Dictumst quisque sagittis purus sit amet. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit.\nPharetra et ultrices neque ornare aenean euismod elementum nisi quis. Amet nulla facilisi morbi tempus iaculis. Neque laoreet suspendisse interdum consectetur libero id faucibus. Pellentesque id nibh tortor id aliquet lectus proin. Aliquet lectus proin nibh nisl. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.",
+  city: "A'ali",
+  address: "A'ali Sports Club\nA'ali",
+  location: {
+    lat: 26.1643456,
+    lng: 50.5081951,
+  },
+};
+export default data;
